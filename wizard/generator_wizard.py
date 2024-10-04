347aff797d627e6fbd64f7f2659f6a977f108c49
Carlos Capote Pérez-Andreu <carlos.capote@hawara.es>
2024-10-04 21:19:14 +0100

    set a maximum number of tolerated errors

--- wizard/generator_wizard.py
+++ wizard/generator_wizard.py
@@ -1,4 +1,5 @@
 from odoo import fields, models
+from odoo.exceptions import ValidationError
 
 class GeneratorWizard(models.TransientModel):
     _name = 'faker.generator.wizard'
@@ -11,9 +12,28 @@ class GeneratorWizard(models.TransientModel):
             return context['generator_id']
 
     generator_id = fields.Many2one('faker.generator', string='Generator', required=True, default=default_generator_id)
-    records = fields.Integer(string='Number of records', required=True, default=100)
+    records = fields.Integer(string='Number of records', required=True, default=25)
+    max_errors = fields.Integer(string='Error tolerance', required=True, default=3)
 
     def action_generate_records(self):
-        for _ in range(self.records):
-            self.generator_id.generate_and_save()
-            self.env.cr.commit()
+        generated_records = 0
+        raised_errors = 0
+        errors = []
+
+        while generated_records < self.records and raised_errors < self.max_errors:
+            try:
+                self.generator_id.generate_and_save()
+                generated_records += 1
+            except Exception as e:
+                raised_errors += 1
+                error = str(e)
+                if not error in errors:
+                    errors.append(error)
+                continue
+
+        if raised_errors >= self.max_errors:
+            raise ValidationError(
+                "\n".join(errors) + "\n\n" +
+                "The maximum accepted number of errors was reached.\n\n"
+                "Please, check your generators and try again."
+            )
